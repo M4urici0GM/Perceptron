3e3e1686a3ce6acd658a9f4a8f4592b305791b08
Mauricio Barbosa <mgbftw@gmail.com>
2020-07-05 00:38:51 -0300

    Implemented traning function, need to refactor though

---
+++ cmake-build-debug/CMakeFiles/Perceptron.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Perceptron.dir/main.cpp.obj"
+  "Perceptron.exe"
+  "Perceptron.exe.manifest"
+  "Perceptron.pdb"
+  "libPerceptron.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Perceptron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
